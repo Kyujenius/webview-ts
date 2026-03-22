0e8bd7f319e50b4252ad91714ed40da379a4d71a
홍규진 <hkj0206@dgu.ac.kr>
2026-03-23 00:08:02 +0900

    test(devtools): mocking vi

--- packages/devtools/src/createDevTools.test.ts
+++ packages/devtools/src/createDevTools.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, vi } from 'vitest';
 import { createDevTools } from './createDevTools';
 import { DevToolsMiddleware } from './middleware/DevToolsMiddleware';
 import { TimeTracker } from './middleware/TimeTracker';
